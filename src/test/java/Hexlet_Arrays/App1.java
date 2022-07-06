61dfafba00c858a960b74e21123199587f04bd4f
Sergey Raklov <ra1976@inbox.ru>
2022-07-06 09:24:26 +0300

    add two metods addPrefix and calculateSum

--- src/test/java/Hexlet_Arrays/App1.java
+++ src/test/java/Hexlet_Arrays/App1.java
@@ -67,11 +67,47 @@ class App1 {
 
     }
 
+    public static String[] addPrefix(String[] names, String prefix) {
+
+        String[] prefixNames = new String[names.length];
+
+        for (var i = 0; i < names.length; i++) {
+
+            prefixNames[i] = prefix + " " + names[i];
+
+        }
+        return prefixNames;
+    }
+
+    public static int calculateSum(int[] numbers1) {
+
+        var sum = 0;
+
+        for (var i = 0; i < numbers1.length; i++) {
+
+            if (numbers1[i] % 3 == 0) {
+
+                sum += numbers1[i];
+
+            }
+
+        }
+        return sum;
+
+    }
+
 
     public static void main(String[] args) {
         int[] numbers = {1, 2, 3, 4, 5, 6};
         var result2 = App1.swap(numbers);
         System.out.println(Arrays.toString(result2));
+
+        String[] names = {"John", "Smit", "Karl"};
+        var namesWithPrefix = App1.addPrefix(names, "Mr.");
+        System.out.println(Arrays.toString(namesWithPrefix));
+
+        int[] numbers1 = {};
+        System.out.println(App1.calculateSum(numbers1));
 
 
     }
