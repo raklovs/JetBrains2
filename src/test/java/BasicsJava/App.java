dc31292dca569d93b403739d842a0ec911af3116
Sergey Raklov <ra1976@inbox.ru>
2022-07-08 09:11:03 +0300

    add metod isPowerThree

--- src/test/java/BasicsJava/App.java
+++ src/test/java/BasicsJava/App.java
@@ -1,7 +1,7 @@
 package BasicsJava;
 
 import java.util.Arrays;
-
+import org.apache.commons.lang3.StringUtils;
 public class App {
     public static int bigLettersCount(String str) {
 
@@ -25,10 +25,43 @@ public class App {
         return App.bigLettersCount(str1) > App.bigLettersCount(str2);
     }
 
+    public static boolean isPowerOfThree(long number) {
+
+
+        if (number < 1) {
+
+            return false;
+        }
+
+        if (number == 1) {
+
+            return true;
+        }
+
+        for (var i = 0; number % 3 == 0; i++) {
+
+            number /= 3;
+
+        }
+        return number == 1;
+    }
+
+//    var current = 1;
+//        while (current <= number) {
+//        if (current == number) {
+//            return true;
+//        }
+//        current *= 3;
+//    }
+//
+//        return false;
+
+
     public static void main(String[] args) {
 
-        var result3 = App.bigLettersCount("Hexlet");
+        var result3 = App.bigLettersCount("HexLet");
         System.out.println(result3);
+        System.out.println(App.isPowerOfThree(6));
 
     }
 }
