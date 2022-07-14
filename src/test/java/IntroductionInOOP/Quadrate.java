d4ad6f1a0830dc320d2f4fa8e8d07944a13a9f20
Sergey Raklov <ra1976@inbox.ru>
2022-07-14 09:34:58 +0300

    add Quadrate.java

--- src/test/java/IntroductionInOOP/Quadrate.java
+++ src/test/java/IntroductionInOOP/Quadrate.java
@@ -1,2 +1,20 @@
-package IntroductionInOOP;public class Quadrate {
-}
+package IntroductionInOOP;
+
+    class Quadrate implements Geometric {
+
+        private int sideA;
+
+        Quadrate(int sideA) {
+            this.sideA = sideA;
+        }
+
+        public String getName() {
+            return "quadrate";
+        }
+
+        public int getSquare() {
+            var square = sideA * sideA;
+            return square;
+        }
+
+    }
